61a38a276acd8640f809c05d4f7005b8c6910316
m4teo <m4teoarg@gmail.com>
2024-04-19 21:43:09 -0300

    init

---
+++ nvim/lua/chadrc.lua
@@ -0,0 +1,23 @@
+-- This file  needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "catppuccin",
+  cmp = {
+    style = "atom",
+  },
+
+  statusline = {
+    theme = "minimal",
+    separator_style = "round"
+  }
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+return M
